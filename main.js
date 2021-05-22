99875fccb764123de6035121b2152d9127fddc63
Aashi858 <72612631+Aashi858@users.noreply.github.com>
2021-05-22 17:31:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+noseX = 0;
+noseY = 0;
+leftWrist = 0;
+rightWrist = 0;
+difference = 0;
+word = "";
+
 function preload(){}
 function setup(){
     canvas = createCanvas(500,400);
@@ -12,6 +19,11 @@ function setup(){
 }
 function draw(){
     background("#e6b845");
+
+    text(word, noseX, noseY);
+    fill("#ed4e4e");
+    textSize(difference);
+    document.getElementById("size").innerHTML = difference;
 }
 function model_loaded(){
     console.log("Model is Loaded")
@@ -19,5 +31,21 @@ function model_loaded(){
 function got_poses(results){
     if(results.length > 0){
         console.log(results);
+
+        noseX = results[0].pose.nose.x;
+        console.log("Nose X :" + noseX);
+        noseY = results[0].pose.nose.y;
+        console.log("Nose Y :" + noseY);
+
+        leftWrist =  results[0].pose.leftWrist.x;
+        rightWrist =  results[0].pose.rightWrist.x;
+        console.log("Left_Wrist :" + leftWrist + "," + "Right_Wrist" + rightWrist);
+        difference = floor(leftWrist - rightWrist);
+        console.log("Difference :" + difference);
     }
+}
+function display(){
+    word = document.getElementById("word").value;
+    console.log("Word :" + word);
+    document.getElementById("word").value = "";
 }
